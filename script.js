fd07998b88a59dad68f30d6ec2bb3909e74aa89e
HaoVo123-hihi <84260715+HaoVo123-hihi@users.noreply.github.com>
2021-11-19 20:28:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ function move(elm) {
     elm.style.left = Math.floor(Math.random()* 90 + 5 ) + '%'
 }
 
-var btn = document.querySelector('#no');
-btn.addEventListener('mouseover', function(e) { 
-    move(e.target)
-})
 
 var btn = document.querySelector('#yes');
 btn.addEventListener('click', function() {
